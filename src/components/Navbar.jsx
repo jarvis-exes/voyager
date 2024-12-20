124921889a59a4e41a322326411bcd117c554fba
Shubham Sharma <8696771553shubham@gmail.com>
2024-12-20 10:38:32 +0000

    Working on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useRef } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+
 const Navbar = () => {
   const navContainerRef = useRef(null);
 
@@ -10,7 +12,7 @@ const Navbar = () => {
       ref={navContainerRef}
       className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6"
     >
-      <header className="absolute top-1/2 w-full-translate-y-1/2">
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between p-4">
           <div className="flex items-center gap-7">
             <img src="/img/logo.png" alt="logo" className="w-10" />
@@ -21,7 +23,20 @@ const Navbar = () => {
               containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
             />
           </div>
-          <div className="flex h-full items-center"></div>
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+                <a
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+            <button className=""></button>
+          </div>
         </nav>
       </header>
     </div>
